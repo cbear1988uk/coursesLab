ec7b9db11736995620647c7fefcf1394f76b7c1e
Collin Bull <vhiessumaniac@hotmail.com>
2019-04-04 13:51:50 +0100

    get booking by date added

--- src/main/java/com/codeclan/example/courseLab/repositories/BookingRepository/BookingRepository.java
+++ src/main/java/com/codeclan/example/courseLab/repositories/BookingRepository/BookingRepository.java
@@ -4,6 +4,11 @@ import com.codeclan.example.courseLab.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long>, BookingRepositoryCustom {
+
+    List<Booking> findBookingByDate(String date);
+
 }
